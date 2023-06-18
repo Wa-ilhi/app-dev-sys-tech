fef4175d24b448165e7f70eca4035017c3f1d4df
Wa-ilhi <128329982+Wa-ilhi@users.noreply.github.com>
2023-06-18 16:41:18 +0800

    Update RecipeIndex.js

--- Homepage/RecipeIndex.js
+++ Homepage/RecipeIndex.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+// Component that represents an index of recipes
 const RecipeIndex = ({ inorderlistsIndex }) => {
+    // Array of alphabetical order lists
     const inorderlists = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-    var num = 0;
+    var num = 0;    // Variable to track a unique key value
     return (
         <>
+             // Iterate over each item in the inorderlists array 
             {
                 inorderlists.map(item => {
                     return (
