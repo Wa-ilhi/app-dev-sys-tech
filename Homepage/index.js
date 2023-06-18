825482ae08d3427da5d75d709fc8fdbfd2f6ac8d
Wa-ilhi <128329982+Wa-ilhi@users.noreply.github.com>
2023-06-18 16:11:02 +0800

    Update index.js

--- Homepage/index.js
+++ Homepage/index.js
@@ -1,30 +1,76 @@
 import React from 'react';
 import { userData } from '../../help';
-import { Row, Col, NavLink, Button } from 'reactstrap';
+import { Col, NavLink, Button, Nav, NavbarText, Navbar, NavbarBrand, NavbarToggler, NavItem, Collapse } from 'reactstrap';
+import MealLists from './MealLists';
+import RecipeIndex from './RecipeIndex';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-//retrieve user data and display
+
 const Home = () => {
   const { username } = userData();
+  const [url, setUrl] = useState("https:/www.themealdb.com/api/json/v1/1/search.php?f=a");
+  const [item, setItem] = useState();
+  const [show, setShow] = useState(false);
+  const [search,setSearch]=useState();
+
+  useEffect(() => {
+    fetch(url).then(res => res.json()).then(data => {
+      console.log(data.meals);
+      setItem(data.meals);
+      setShow(true);
+
+    })
+  }, [url])
+
+  const searchRecipe=(evt)=>{
+    setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+}
+
+   const setIndex = (inorderlists) => {
+    setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${inorderlists}`)
+  }
+
+
 
   return (
-    <Row className="home">
-      <Col sm="12" md={{ size: 4, offset: 4 }}>
-        <h2 style={{ color: "#fff" }}>
-          Welcome to Strapi <span style={{ color: "#007bff" }}>{username}</span>
-        </h2>
-        <p style={{ color: "#fff" }}>
-          We hope you are making progress on your project!
-        </p>
-        <Col sm="12" md={{ size: 4, offset: 4 }}>
-          <Button color="primary">
-            <NavLink href="/logout">
-              <h3 style={{ color: "#000" }}>Logout</h3>
-            </NavLink>
-          </Button>
-        </Col>
-      </Col>
-    </Row>
+    <div>
+      <Navbar color="primary" dark expand="md">
+        <NavbarBrand href="/">
+          <h4>Welcome to FoodBites, <span style={{ color: "#000000" }}>{username}</span></h4>
+        </NavbarBrand>
+        <Collapse navbar></Collapse>
+        <NavbarText>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+            <h6 color='primary'><NavLink href="/logout">Logout</NavLink></h6>
+            </NavItem>
+          </Nav>
+        </NavbarText>
+      </Navbar>
+
+      <div className="main">
+        <div className="heading">
+          <h3>Be the chef in your own kitchen!</h3>
+     
+        </div>
+
+        <div className="search-bar">         
+        <input type="search" className="search-bar" onChange={e=> setSearch(e.target.value)} onKeyPress={searchRecipe}
+          placeholder='Enter Food Name '/>
+        </div>
+        <div className='container'>
+          {
+            show ? <MealLists data={item} /> : "Not found"
+          }
+        </div>
+        <div className="indexContainer">
+          <RecipeIndex inorderlistsIndex={(inorderlists) => setIndex(inorderlists)} />
+        </div>
+      </div>
+    </div>
   );
 };
+
 
 export default Home;
