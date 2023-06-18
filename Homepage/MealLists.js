dae108de932ced1720c3441be071851fab97c986
Wa-ilhi <128329982+Wa-ilhi@users.noreply.github.com>
2023-06-18 16:14:03 +0800

    Update MealLists.js

--- Homepage/MealLists.js
+++ Homepage/MealLists.js
@@ -1,1 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
+// This portion 
+const MealLists = ({ data }) => {
+    console.log(data)
+    let navigate = useNavigate();
+    return (
+        <>
+            {
+                (!data) ? "No Recipe Available" : data.map(item => {
+                    return (
+                        <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>
+                            <img src={item.strMealThumb} alt="" />
+                            <h4>{item.strMeal}</h4>
+                        </div>
+                    )
+                })
+            }
+
+        </>
+    )
+};
+
+
+export default MealLists;
