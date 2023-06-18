7c76357e7f46e5522d0f7b5f47bface22fce5499
Wa-ilhi <128329982+Wa-ilhi@users.noreply.github.com>
2023-06-18 16:38:25 +0800

    Update MealLists.js

--- Homepage/MealLists.js
+++ Homepage/MealLists.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+// Component that displays a list of meals
 const MealLists = ({ data }) => {
-    console.log(data)
-    let navigate = useNavigate();
+    console.log(data)   // Log the data prop
+    let navigate = useNavigate();   // Hook from react-router-dom for navigation
     return (
         <>
-            {
+                  // Display a message if data is not available
+            {   
                 (!data) ? "No Recipe Available" : data.map(item => {
                     return (
-                        <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>
-                            <img src={item.strMealThumb} alt="" />
-                            <h4>{item.strMeal}</h4>
+                        // Display a card for each item in the data array
+                        <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>  //The key attribute is used to keep track of individual elements in a list
+                            <img src={item.strMealThumb} alt="" />  // This renders an img element that displays the meal thumbnail.                        
+                            <h4>{item.strMeal}</h4> 
                         </div>
                     )
                 })
@@ -21,5 +24,5 @@ const MealLists = ({ data }) => {
     )
 };
 
-
+// Exporting the MealLists component
 export default MealLists;
